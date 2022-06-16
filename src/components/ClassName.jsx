d907830e1c47bd98b9113c54a1d49d8fe5569ae1
Florian Rouanet <rouanetflorian@gmail.com>
2022-06-16 16:00:17 +0200

    add other render for classNameComponent

--- src/components/ClassName.jsx
+++ src/components/ClassName.jsx
@@ -1,33 +1,47 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../style/Components.css'
 
 export default function ClassName() {
   const arrCn = ['cn-bold', 'cn-primary', 'cn-background', 'cn-lead']
+  const [showHide, setShowHide] = useState(true)
 
-  const TrueRender = ({ enabled }) => {
+  const RenderClass = ({ enabled }) => {
     return (
-      <div className={enabled ? 'cn-enabled' : ''}>Conditionnal Class True</div>
-    )
-  }
-
-  const FalseRender = ({ enabled }) => {
-    return (
-      <div className={enabled ? 'cn-enabled' : null}>
-        Conditionnal Class False
+      <div>
+        <div className={enabled ? 'cn-enabled' : ''}>Conditionnal Class Active</div>
+        <div className={enabled ? '' : 'cn-enabled'}>
+          Conditionnal Class Inactive
+        </div>
       </div>
     )
   }
 
   return (
     <>
-      <div>
-        <h2 className="m-4">
+      <div className="d-flex flex-column">
+        <h2 className="text-success m-5 fw-bold">
           Manière de traiter les classes CSS dans un composant React
         </h2>
         <p className={arrCn.join(' ')}>Array of class attributs</p>
 
-        <TrueRender enabled={true} />
-        <FalseRender enabled={false} />
+        <div className="mx-auto m-2 d-grid gap-3">
+          <button
+            className="btn btn-warning mx-auto"
+            onClick={() => setShowHide(true)}
+          >
+            Afficher
+          </button>
+          {showHide ? <h3 text-center>Hello World !</h3> : null}
+          <button
+            className="btn btn-warning mx-auto"
+            onClick={() => setShowHide(false)}
+          >
+            Cacher
+          </button>
+
+          <RenderClass enabled={true} />
+        </div>
 
         <Link className="d-flex" to={'/'}>
           <button className="btn-primary m-5">Retour</button>
